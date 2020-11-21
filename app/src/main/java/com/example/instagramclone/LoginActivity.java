812e0c788c5053288873e63bfa64e0116df53d35
acacia1021 <acacia.lord@faulkner.edu>
2020-11-20 19:36:28 -0600

    Walkthrough File - log in log out, & register workin'

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -47,8 +47,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
                 public void onClick(View v){
             Log.i(TAG, "onClick login button");
-            String username = etUsername.getText().toString();
-            String password = etPassword.getText().toString();
+            final String username = etUsername.getText().toString();
+            final String password = etPassword.getText().toString();
             loginUser(username, password);
         }
     });
@@ -65,21 +65,23 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-private void loginUser(String username, final String password){
+private void loginUser(String username, String password){
     Log.i(TAG, "attempting to login user" + username);
     //TODO: navigate to the main activity if the user has signed in properly
     ParseUser.logInInBackground(username, password, new LogInCallback() {
         @Override
         public void done(ParseUser user, ParseException e) {
-            if (e != null){
-                //TODO: better error handling
-                Log.e(TAG, "Issue with login", e);
-                Toast.makeText(LoginActivity.this, "Issue with login", Toast.LENGTH_SHORT).show();
-                return;
+            if(e == null){
+                Log.d("LoginActivity", "Login successful");
+                final Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
-            //TODO: navigate to the main activity if the user has signed in properly
-            goMainActivity();
-            Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            else{
+                Log.e("LoginActivity", "Login failure");
+                Toast.makeText(LoginActivity.super.getBaseContext(), "Username or password incorrect", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
         }
     });
     }
